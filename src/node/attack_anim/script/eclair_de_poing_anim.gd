ad521811119267a54c5531ef78edada303e738b7
LeoAsahi <leonardo.bertonasco01@gmail.com>
2026-03-01 23:38:31 +0100

    feat(EclairDePoing): add eclair de poing

---
+++ src/node/attack_anim/script/eclair_de_poing_anim.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func play_attack(sender : PokemonNode, receiver : PokemonNode, battleui : BattleUI):
+	global_position = receiver.global_position
+	visible = true
+	$AnimationPlayer.play("RESET")
+	SoundManager.play_sfx(preload("res://sound/SFX/attack_sfx/Thunder Punch.mp3"), -10)
+	battleui.flash_screen(2, Color(1, 1, 0, 0.0))
+	await $AnimationPlayer.animation_finished
+
+func setup_anim() ->void :
+	visible = false
